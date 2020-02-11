a204fc59e2d79a58117d9eb20c66b8a311dc8a24
Melanie-Carle <36747263+Melanie-Carle@users.noreply.github.com>
2020-02-10 18:42:33 -0800

    Initialization

---
+++ Views/Partials/Header.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<html>
+<head>
+	
+	<div class="NavBar"></div>
+
+</head>
+</html>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+	
+	<link href="CSS/Master.css" rel="stylesheet" type="text/css">
+	<?php
+		include("Views/Partials/Header.php");
+		include("Javascript/connections/req.php");
+	?>	
+	
+
+	
+</head>
+
+<body>
+
+</body>
+</html>
