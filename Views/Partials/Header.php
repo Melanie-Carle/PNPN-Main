35e1cd9a500051ed94b75a5b4aef1171de0ba54d
Melanie-Carle <36747263+Melanie-Carle@users.noreply.github.com>
2020-03-02 17:20:30 -0800

    Edits

--- Views/Partials/Header.php
+++ Views/Partials/Header.php
@@ -1,8 +1,74 @@
 <!doctype html>
 <html>
-<head>
-	
-	<div class="NavBar"></div>
+	<head>
+		<nav class="navbar navbar-expand-lg" id="NavBar">
+			<div class="container-fluid">
+				<a class="navbar-brand" id="NavBarName">Pacific Northwest Pirate Nation</a>
+				<ul class="navbar-nav" id="NavBarLinks">
+					<li class="nav-item">
+	  					<a class="nav-link" href="/PNPN-Official-Website/index.php">Home</a>
+					</li>
 
-</head>
+					<li class="nav-item dropdown">
+	     		 		<a class="nav-link" id="navbardrop" data-toggle="dropdown" role = "button">
+	     		 			Events
+	      				</a>
+					    <div class="dropdown-menu">
+					    	<a class="dropdown-item" href="/PNPN-Official-Website/RoT.php">Roast of Tortuga</a>
+					        <a class="dropdown-item" href="#">Port Nassau</a>
+					        <a class="dropdown-item" href="#">Tortuga Nights</a>
+					        <a class="dropdown-item" href="#">Port in the Storm</a>
+					    </div>
+	    			</li>
+
+					<li class="nav-item dropdown">
+	     		 		<a class="nav-link" id="navbardrop" data-toggle="dropdown">
+	     		 			Community
+	      				</a>
+					    <div class="dropdown-menu">
+					    	<a class="dropdown-item" href="#">Crews</a>
+					        <a class="dropdown-item" href="#">Social Media</a>
+					    </div>
+	    			</li>
+
+	    			<li class="nav-item dropdown">
+	     		 		<a class="nav-link" id="navbardrop" data-toggle="dropdown">
+	     		 			Command Center
+	      				</a>
+					    <div class="dropdown-menu">
+					    	<a class="dropdown-item" href="#">About</a>
+					        <a class="dropdown-item" href="#">Staff</a>
+					        <a class="dropdown-item" href="#">History</a>
+					        <a class="dropdown-item" href="#">Event Layout</a>
+					    </div>
+	    			</li>
+
+	    			<li class="nav-item dropdown">
+	     		 		<a class="nav-link" id="navbardrop" data-toggle="dropdown">
+	     		 			FAQ/Rules
+	      				</a>
+					    <div class="dropdown-menu">
+					    	<a class="dropdown-item" href="#">F.A.Q.</a>
+					        <a class="dropdown-item" href="#">Rules</a>
+					        <a class="dropdown-item" href="#">Etiquette</a>
+					        <a class="dropdown-item" href="#">Theme</a>
+					    </div>
+	    			</li>
+
+	    			<li class="nav-item dropdown">
+	     		 		<a class="nav-link" id="navbardrop" data-toggle="dropdown">
+	     		 			Get Involved
+	      				</a>
+					    <div class="dropdown-menu">
+					    	<a class="dropdown-item" href="#">Volunteer</a>
+					        <a class="dropdown-item" href="#">Vendors</a>
+					    </div>
+	    			</li>
+	    			<li class="nav-item">
+	  					<a class="nav-link" href="#">Tickets</a>
+					</li>
+	    		</ul>
+	    	</div>
+		</nav>
+	</head>
 </html>
--- index.php
+++ index.php
@@ -8,11 +8,30 @@
 		include("Javascript/connections/req.php");
 	?>	
 	
-<p>meow</p>
-	
 </head>
 
 <body>
+	<div class = "container">
+		<div class = "d-flex justify-content-center">
+			<div class = "col">
+				<div id="CarouselControls" class="carousel slide" data-ride="carousel">
+				  <div class="carousel-inner">
+				    <div class="carousel-item active">
+				      <img class="d-block w-100" src="CSS/styles/PNSR20.jpg">
+				    </div>
+				  </div>
+				  <a class="carousel-control-prev" href="#CarouselControls" role="button" data-slide="prev">
+				    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				    <span class="sr-only">Previous</span>
+				  </a>
+				  <a class="carousel-control-next" href="#CarouselControls" role="button" data-slide="next">
+				    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+				    <span class="sr-only">Next</span>
+				  </a>
+				</div>
+			</div>
+		</div>
+	</div>
 
 </body>
 </html>
